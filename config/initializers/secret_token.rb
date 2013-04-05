7ab20f3089369f185a323ab7fa70ff23af9fef64
Peter Wang <happypeter1983@gmail.com>
2013-04-05 11:03:21 +0800

    Revert "tranfer to rails app, welcome out new home page"
    This reverts commit 02a6d01faf11ab86b4a966a85c2c090ad6e63cff.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Tcool::Application.config.secret_token = '84681216d2f849d7ae5451ede7cad3175d1d9faa3569774c8577bd06f86ba3a48b68cb43a1350f1bed4a8d19d5d7b77f986bef1ede28ee0541738d51f8ca36f6'
